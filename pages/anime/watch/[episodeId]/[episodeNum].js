6352204777638e2693c4e63d5e5f2fa72b3c137d
Fxcking Angel <70914843+FxckingAngel@users.noreply.github.com>
2024-08-02 08:32:08 -0500

    Update [episodeNum].js
    Signed-off-by: Fxcking Angel <70914843+FxckingAngel@users.noreply.github.com>

--- pages/anime/watch/[episodeId]/[episodeNum].js
+++ pages/anime/watch/[episodeId]/[episodeNum].js
@@ -4,11 +4,13 @@ import Head from "next/head";
 import { useRouter } from 'next/router';
 import MainLayout from "../../../../components/ui/MainLayout";
 import Link from "next/link";
+import { useQuery } from 'react-query';
 import { getAnimeDetails, getAnimeEpisodeData, getAnimeEpisodeLinks, getExternalLink } from "../../../../src/handlers/anime";
 import AnimeDetails from "../../../../components/anime/info/AnimeDetails";
 import EpisodesList from "../../../../components/anime/player/EpisodesList";
 import PrimaryButton from "../../../../components/buttons/PrimaryButton";
 import { HiOutlineDownload } from "react-icons/hi";
+import { BsFillPlayFill } from 'react-icons/bs';
 
 const VideoPlayer = dynamic(() => import('./../../../../components/anime/player/VideoPlayer'), { ssr: false, loading: () => <div>Loading...</div> });
 
@@ -16,9 +18,7 @@ export const getServerSideProps = async (context) => {
   const { episodeId, episodeNum, dub = false } = await context.query;
 
   const anime = await getAnimeDetails(episodeId);
-
   const episode = await getAnimeEpisodeData(episodeId);
-
   const episodeNumber = parseInt(episodeNum);
 
   return {
@@ -51,28 +51,29 @@ function StreamingPage({ episode, anime, episodeNumber, dub }) {
     };
 
     fetchDownloadLink();
-  }, [episode, episodeNumber]);
+  }, [episodeName]);
 
-   if (!episodeId) {
-     return <MainLayout>loading...</MainLayout>;
-   }
+  if (!episode) {
+    return <MainLayout>Loading...</MainLayout>;
+  }
 
-   const { episode, isLoading, isError, error } = useQuery("details", () =>
-     getStreamLink(episodeId)
-   );
+  const { data: streamLink, isLoading, isError, error } = useQuery(["details", episodeName], () =>
+    getAnimeEpisodeLinks(episodeName)
+  );
 
-   const handleVLCPlayer = () => {
-     window.open(
-      `intent:${episode?.sources[0].file}#Intent;scheme=vlc;package=org.videolan.vlc;end`,
+  const handleVLCPlayer = () => {
+    window.open(
+      `intent:${streamLink?.sources[0].file}#Intent;scheme=vlc;package=org.videolan.vlc;end`,
       "_blank",
-     );
-   };
-   const handleMxPlayer = () => {
-     window.open(
-       `intent:${episode?.sources[0].file}#Intent;scheme=mxplayer;package=com.mxtech.videoplayer.ad;end`,
-     "_blank",
-     );
-   };
+    );
+  };
+  
+  const handleMxPlayer = () => {
+    window.open(
+      `intent:${streamLink?.sources[0].file}#Intent;scheme=mxplayer;package=com.mxtech.videoplayer.ad;end`,
+      "_blank",
+    );
+  };
 
   return (
     <>
@@ -80,10 +81,7 @@ function StreamingPage({ episode, anime, episodeNumber, dub }) {
         <title>{"Watch " + (anime?.title.english || anime?.title.romaji) + " Episode " + episodeNumber + " - Munchyroll "}</title>
         <meta name="description" content={anime?.synopsis} />
         <meta name="keywords" content={anime?.genres} />
-        <meta
-          property="og:title"
-          content={"Watch " + (anime?.title.english || anime?.title.romaji) + " Episode " + episodeNumber + " - Munchyroll "}
-        />
+        <meta property="og:title" content={"Watch " + (anime?.title.english || anime?.title.romaji) + " Episode " + episodeNumber + " - Munchyroll "} />
         <meta property="og:description" content={anime?.description} />
         <meta property="og:image" content={anime?.image} />
         <meta name="theme-color" content={anime?.color} />
@@ -97,26 +95,25 @@ function StreamingPage({ episode, anime, episodeNumber, dub }) {
         <MainLayout useHead={false} type={"anime"}>
           <div className="mt-3 lg:flex lg:space-x-4 rounded-xl">
             <div className="alignfull w-full overflow-hidden max-w-screen-xl rounded-xl">
-               {isDubbed ? (
+              {isDubbed ? (
                 <VideoPlayer videoSource={dubbedEpisodeDataLink} key={dubbedEpisodeDataLink} className="rounded-xl " />
               ) : (
-                <VideoPlayer videoSource={episodeDataLink} key={episodeDataLink} className="rounded-xl " />
+                <VideoPlayer videoSource={streamLink?.sources[0].file} key={streamLink?.sources[0].file} className="rounded-xl " />
               )}
               <VideoPlayer episodeNumber={episodeNumber} episodeTitle={episodeTitle} episodeName={episodeName} className="rounded-xl " />
 
               <div className="flex justify-between pt-5">
-                  <Link className={`justify-start ${(episodeNumber > 1) ? "" : "invisible"}`} href={`/anime/watch/${anime.id}/${episodeNumber - 1}`}>
-                    <button title="Go to the previous episode" className="bg-[#2f6b91] hover:bg-[#214861] transition-all text-white font-bold m-4 py-2 px-4 rounded" onClick={() => { handlePreviousEpisode(); setIsLoading(true); }}>&#x2190; Episode {episodeNumber - 1} </button>
-                  </Link>
-                  <Link className={`justify-end ${(episodeNumber < episode.length) ? "" : "invisible"}`} href={`/anime/watch/${anime.id}/${episodeNumber + 1}`}>
-                    <button title="Go to the next episode" className="bg-[#2f6b91] hover:bg-[#214861] transition-all text-white font-bold m-4 py-2 px-4 rounded" onClick={() => { handleNextEpisode(); setIsLoading(true); }}>Episode {episodeNumber + 1} &#x2192;</button>
-                  </Link>
-                </div>
+                <Link className={`justify-start ${(episodeNumber > 1) ? "" : "invisible"}`} href={`/anime/watch/${anime.id}/${episodeNumber - 1}`}>
+                  <button title="Go to the previous episode" className="bg-[#2f6b91] hover:bg-[#214861] transition-all text-white font-bold m-4 py-2 px-4 rounded">&#x2190; Episode {episodeNumber - 1}</button>
+                </Link>
+                <Link className={`justify-end ${(episodeNumber < episode.length) ? "" : "invisible"}`} href={`/anime/watch/${anime.id}/${episodeNumber + 1}`}>
+                  <button title="Go to the next episode" className="bg-[#2f6b91] hover:bg-[#214861] transition-all text-white font-bold m-4 py-2 px-4 rounded">Episode {episodeNumber + 1} &#x2192;</button>
+                </Link>
+              </div>
               <div className="pt-5 font-bold max-lg:text-center sm:block mb-5">
                 <div className="dark:text-secondary text-primary capitalize space-y-2">
                   <Link className="hover:text-blue-400 transition sm:text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl " href={`/anime/info/${anime?.id}`}>{anime?.title.english || anime?.title.romaji}</Link>
                   <p className="dark:text-secondary text-primary sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">{episodeTitle ? (`Episode ${episodeNumber}: ${episodeTitle}`) : (`Episode ${episodeNumber}`)}</p>
-
                 </div>
               </div>
               <AnimeDetails animeData={anime} episodeData={episode} episodePage={true} />
@@ -124,15 +121,14 @@ function StreamingPage({ episode, anime, episodeNumber, dub }) {
             <EpisodesList episodeData={episode} episodeName={episodeName} id={anime.id} isDubbed={false} />
           </div>
           <div className="max-w-xs mt-10 space-y-4">
-             {isDubbed ? (
+            {isDubbed ? (
               <PrimaryButton
                 icon={<BsFillPlayFill />}
                 sub="Classic anime experience"
                 onClick={() => {
                   setIsDubbed(false);
                   window.history.replaceState({}, '', `/anime/watch/${anime?.id}/${episodeNumber}?dub=false`);
-                  }
-                }
+                }}
               >
                 Watch Subbed
               </PrimaryButton>
@@ -140,11 +136,10 @@ function StreamingPage({ episode, anime, episodeNumber, dub }) {
               <PrimaryButton
                 icon={<BsFillPlayFill />}
                 sub="English anime experience"
-                  onClick={() => {
-                    setIsDubbed(true);
-                    window.history.replaceState({}, '', `/anime/watch/${anime?.id}/${episodeNumber}?dub=true`);
-                    }
-                  }
+                onClick={() => {
+                  setIsDubbed(true);
+                  window.history.replaceState({}, '', `/anime/watch/${anime?.id}/${episodeNumber}?dub=true`);
+                }}
               >
                 Watch Dubbed
               </PrimaryButton>
